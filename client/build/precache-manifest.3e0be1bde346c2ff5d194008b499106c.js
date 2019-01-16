95390944acbd5b4ae667deb78015869d9cbf7a1a
efrencav <efren@abstract-creative.com>
2019-01-15 22:00:01 -0600

    make it better

---
+++ client/build/precache-manifest.3e0be1bde346c2ff5d194008b499106c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2cf4de7ecbf8e62a62b",
+    "url": "/static/css/main.de147075.chunk.css"
+  },
+  {
+    "revision": "c2cf4de7ecbf8e62a62b",
+    "url": "/static/js/main.c2cf4de7.chunk.js"
+  },
+  {
+    "revision": "154adcf8d6eaaf5e727b",
+    "url": "/static/js/1.154adcf8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "796e3c3914fdc416054fc6d95128070a",
+    "url": "/index.html"
+  }
+];
