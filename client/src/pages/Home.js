18e6a9799f085ee49e676b5653e9746ffee69745
efrencav <efren@abstract-creative.com>
2019-01-15 19:23:03 -0600

    new with emoji

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -36,4 +36,83 @@ class Home extends Component {
         })
         );
     };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.getBooks();
+    };
+
+    handleBookSave = id => {
+        const book = this.state.books.find(book => book.id === id);
+
+        API.saveBook({
+            googleId: book.id,
+            title: book.volumeInfo.title,
+            subtitle: book.volumeInfo.subtitle,
+            link: book.volumeInfo.infoLink,
+            authors: book.volumeInfo.authors,
+            description: book.volume.Info.description,
+            image: book.volumeInfo.imageLinks.thumbnail
+        }).then(() => this.getBooks());
+    };
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                        <Jumbotron>
+                            <h1 className="text-center">
+                                <strong>(React) Google Books Search</strong>
+                            </h1>
+                            <h2 className="text-center">Search for and Save Books of Interest.</h2>
+                        </Jumbotron>
+                    </Col>
+                    <Col size="md-12">
+                        <Card title="Book Search" icon="far fa-book">
+                            <Form
+                            handleInputChange={this.handleInputChange}
+                            handleFormSubmit={this.handleFormSubmit}
+                            q={this.state.q}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        <Card title="Results">
+                            {this.state.books.length ? (
+                                <List>
+                                {this.state.books.map(books => (
+                                    <Book
+                                    key={book.id}
+                                    title={book.volumeInfo.titile}
+                                    subtitle={book.volumeInfo.subtitle}
+                                    link={book.volumeInfo.infoLink}
+                                    authors={book.volumeInfo.authors.join(", ")}
+                                    description={book.volumeInfo.description}
+                                    image={book.volumeInfo.imageLinks.thumbnail}
+                                    Button={() => (
+                                        <button
+                                        onClick={() => this.handleBookSave(book.id)}
+                                        className="btn btn-ormiary ml=2"
+                                        >
+                                        Save
+                                        </button>
+                                    )}
+                                    />
+                                ))}
+                                </List>
+                            ) : (
+                                <h2 className="text-center">{this.state.message}</h2>
+                            )}
+                        </Card>
+                    </Col>
+                </Row>
+                <Footer />
+            </Container>
+        );
+    }
 }
+
+export default Home;
